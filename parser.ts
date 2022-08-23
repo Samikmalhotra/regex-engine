81fd5b5cbb0802c4126748e35016cafc20159a53
Samik Malhotra <samikmalhotra66@gmail.com>
2022-08-23 17:11:30 +0530

    parser

--- parser.ts
+++ parser.ts
@@ -1,4 +1,4 @@
-const insertConcatOperator = (exp: string): string => {
+export const insertConcatOperator = (exp: string): string => {
   let output = "";
   for (let i = 0; i < exp.length; i++) {
     const token = exp[i];
@@ -17,6 +17,52 @@ const insertConcatOperator = (exp: string): string => {
 
       output = output + ".";
     }
+  }
+
+  return output;
+};
+
+const peek = (stack: any[]) => {
+  return stack.length && stack[stack.length - 1];
+};
+
+const operatorPrecedence = {
+  "|": 0,
+  ".": 1,
+  "*": 2,
+};
+
+export const toPostFix = (exp: string) => {
+  let output = "";
+  const operatorStack = [];
+
+  for (const token of exp) {
+    if (token === "." || token === "|" || token === "*") {
+      while (
+        operatorStack.length &&
+        peek(operatorStack) !== "(" &&
+        operatorPrecedence[peek(operatorStack)] >= operatorPrecedence[token]
+      ) {
+        output += operatorStack.pop();
+      }
+
+      operatorStack.push(token);
+    } else if (token === "(" || token === ")") {
+      if (token == "(") {
+        operatorStack.push(token);
+      } else {
+        while (peek(operatorStack) !== "(") {
+          output += operatorStack.pop();
+        }
+        operatorStack.pop();
+      }
+    } else {
+      output += token;
+    }
+  }
+
+  while (operatorStack.length) {
+    output += operatorStack.pop();
   }
 
   return output;
